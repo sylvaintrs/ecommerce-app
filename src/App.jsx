919987a5a1ead69b27f60ad16444d847e13e019b
Sylvain.trs <sylvain.tremblais41@gmail.com>
2022-05-03 09:37:41 +0200

    update-code-structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './sass/main.css';
+
+import NavBar from './components/NavBar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+
+      <div className="background-rectangle"></div>
+    </div>
+  );
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Provider } from 'react-redux'
-import store from './store'
-
 import App from './App';
+
+import { Provider } from 'react-redux'
+
+import store from './redux/Reducer'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
   <Provider store={store}>
-    <App />
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Provider>
 );
